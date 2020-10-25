f8d567d40cd5f30d004acda45cf10ce87584a491
luisanegri <luisaanegri@gmail.com>
2020-10-25 13:25:40 +0100

    scrape urls from books that are in stock

--- pageScraper.js
+++ pageScraper.js
@@ -8,7 +8,17 @@ const scraperObject = {
         console.log(`Navigating to ${this.url}...`);
         // goto navigates to the url
         await page.goto(this.url);
-
+        // Wait for the required DOM to be rendered
+        await page.waitForSelector('.page_inner');
+        // Get the link to all the required books
+        let urls = await page.$$eval('section ol > li', links => {
+            // Make sure the book to be scraped is in stock
+            links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
+            // Extract the links from the data
+            links = links.map(el => el.querySelector('h3 > a').href)
+            return links;
+        });
+        console.log(urls);
     }
 }
 
